7756711033205cf04502c07c9a2d724e645d6fba
Wardayaz <wayaz107@gmail.com>
2021-06-22 20:50:20 -0500

    create recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,16 @@
 class RecipesController < ApplicationController
+    def index
+    end 
+
+    def show
+    end 
+
+    def create
+    end 
+
+    def update
+    end 
+
+    private 
+    
 end
