976e2dcd486f411a14cca7cf490ff405b2228f6e
Wardayaz <wayaz107@gmail.com>
2021-06-24 08:24:39 -0500

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :recipes
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
 end
