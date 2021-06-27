6b584bb047c3fd530a27f8fee2a00ac91fa7901a
Wardayaz <wayaz107@gmail.com>
2021-06-27 00:48:59 -0500

    update recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,9 @@
 class Recipe < ApplicationRecord
+    belongs_to :user, optional: true
+    has_many :comments
+    has_many :likes
+    has_many :ingredients
+    validates :name, :image_url, :description, :instructions, presence: true
+
+    accepts_nested_attributes_for :ingredients
 end
